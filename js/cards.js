12e483844d31252bcc47abfe704040562e1f3710
Nasif-Hasan <nasifhasan35@gmail.com>
2022-08-22 11:55:16 +0600

    name-section

--- js/cards.js
+++ js/cards.js
@@ -1,6 +1,31 @@
-function selectFive(element){
-    
+const playerArray = []
+
+function display(allPlayers){ 
+
+    const playerList = document.getElementById('selected-players');
+    playerList.innerHTML = '';
+
+    for (let i = 0; i < allPlayers.length; i++) {
+
+        const name = playerArray[i].playerName  
+        
+        const li = document.createElement('li');
+
+        li.innerHTML = `
+        <p>${i + 1}   ${name}</p>
+        `
+        playerList.appendChild(li)
+    }
+}
+
+function selectFive(element){    
     const playerName = element.parentNode.parentNode.children[0].innerText;
 
-    console.log(playerName);
+    const playerObj = {
+        playerName: playerName
+    }
+
+    playerArray.push(playerObj);
+
+    display(playerArray);
 }
