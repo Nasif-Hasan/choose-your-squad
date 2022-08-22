e254a9eeb1cc03436289447e7979789d911461d5
Nasif-Hasan <nasifhasan35@gmail.com>
2022-08-22 19:33:58 +0600

    primary-all-done

--- js/cards.js
+++ js/cards.js
@@ -33,3 +33,50 @@ function selectFive(element){
 
     display(playerArray);
 }
+
+
+function playerTotal(){
+    const playerMoneyField = document.getElementById('per-player');
+    const playerMoneyString = playerMoneyField.value;
+    const playerMoney = parseFloat(playerMoneyString);
+    
+    playerMoneyField.value = '';
+    
+    const playerTotalMoney = playerMoney * playerArray.length;
+    
+    const playerExpenses = document.getElementById('player-total');
+    const playerExpensesString = playerExpenses.innerText;
+    const previousExpensesTotal = parseFloat(playerExpensesString);
+    
+    const newPlayerExpenses = previousExpensesTotal + playerTotalMoney;
+    playerExpenses.innerText = newPlayerExpenses;
+
+}
+
+function totalExpenses(){
+
+    const managerMoneyField = document.getElementById('manager-money');
+    const managerMoneyString = managerMoneyField.value;
+    const managerMoney = parseFloat(managerMoneyString);
+
+    const coachMoneyField = document.getElementById('coach-money');
+    const coachMoneyString = coachMoneyField.value;
+    const coachMoney = parseFloat(coachMoneyString);
+
+    const playerExpenses = document.getElementById('player-total');
+    const playerExpensesString = playerExpenses.innerText;
+    const playersMoney = parseFloat(playerExpensesString);
+
+    managerMoneyField.value = '';
+    coachMoneyField.value = '';
+
+    const totalExpenses = playersMoney + managerMoney + coachMoney;
+
+    const totalExpensesField = document.getElementById('expence-total')
+    const previousTotalExpensesString = totalExpensesField.innerText;
+    const previousTotalMoney = parseFloat(previousTotalExpensesString);
+
+    const newExpensesTotal = previousTotalMoney + totalExpenses;
+    totalExpensesField.innerText = newExpensesTotal
+
+}
